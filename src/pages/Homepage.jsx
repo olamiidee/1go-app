fbb32d08a19be129b91b3b02a0b3c113a43e1272
olamiidee <osibeluwoolamide@gmail.com>
2023-02-02 01:09:04 +0100

    homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,10 +6,18 @@ const Homepage = () => {
       <Header />
       <main className="w-full bg-slate-200">
         <section className="w-full min-h-screen bg-home bg-center bg-no-repeat">
-          <div className="overlay w-full h-screen bg-gray-800/30 flex justify-center items-center">
-            <h1 className="text-[3rem] font-bold text-white">
-              Homepage - Book a Ride
+          <div className="overlay w-full h-screen bg-gray-800/30 flex flex-col justify-center items-start space-y-3 text-white px-[5%] sm:px-[10%]">
+            <h1 className="text-[2.5rem] font-bold uppercase md:text-[3rem]">
+              Beat The Queue!!
             </h1>
+            <div className="pb-3">
+              <p>Get easy rides for as low as ---</p>
+              <p>From Oke-Odo to school park and vice versa</p>
+            </div>
+
+            <button className="bg-blue-500 px-8 py-2 hover:bg-transparent hover:border-blue-500 hover:border-2">
+              Book Now
+            </button>
           </div>
         </section>
         <section className="h-screen">
