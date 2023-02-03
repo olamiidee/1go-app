985f257f3bf3d285ed8836cdc1a293f89f37527b
olamiidee <osibeluwoolamide@gmail.com>
2023-02-03 05:13:22 +0100

    testimonials

---
+++ src/components/TestimonialData.js
@@ -0,0 +1,34 @@
+const TESTIMONIAL = [
+  {
+    picture: "kk",
+    content:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. In et, omnisest cumque quaerat obcaecati illum modi saepe officiis atque, nesciunt",
+    name: "My Name",
+  },
+  {
+    picture: "ll",
+    content:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. In et, omnisest cumque quaerat obcaecati illum modi saepe officiis atque, nesciunt",
+    name: "My Name",
+  },
+  {
+    picture: "mm",
+    content:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. In et, omnisest cumque quaerat obcaecati illum modi saepe officiis atque, nesciunt",
+    name: "My Name",
+  },
+  {
+    picture: "oo",
+    content:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. In et, omnisest cumque quaerat obcaecati illum modi saepe officiis atque, nesciunt",
+    name: "My Name",
+  },
+  {
+    picture: "",
+    content:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. In et, omnisest cumque quaerat obcaecati illum modi saepe officiis atque, nesciunt",
+    name: "My Name",
+  },
+];
+
+export default TESTIMONIAL;
