8d438e08a3cd53930da7af5621d8b0173745b8f8
olamiidee <osibeluwoolamide@gmail.com>
2023-02-04 03:43:02 +0100

    login page fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,25 +17,21 @@ const Login = () => {
         <div className="w-full h-screen bg-white flex flex-col justify-center items-center mt-[80px]">
           <div className="px-6 py-12 w-full">
             <div className="text-center">
-              <h1 className="text-[2rem] font-bold text-black">Login</h1>
-              <p>
-                Don't have an Account?
-                <Link to="/register" className="text-blue-500 cursor-pointer">
-                  {" "}
-                  Sign up
-                </Link>
+              <h1 className="text-[2rem] font-bold text-black">Welcome back</h1>
+              <p className="text-slate-500 font-light">
+                Welcome back,please enter your details
               </p>
             </div>
-            {/* register form */}
+            {/* Login form */}
             <form>
               <div className="font-light space-y-4 py-10 w-full rounded-lg sm:mt-8 sm:px-8 sm:w-[90%] lg:w-[45%] sm:bg-[#EFF6FF] mx-auto">
                 <div>
-                  <label htmlFor="email">Email address</label>
+                  <label htmlFor="email">Email</label>
                   <input
                     className="login-input"
                     id="email"
                     onChange={handleLoginChange}
-                    placeholder="Enter your email adress"
+                    placeholder="Enter your email"
                     type="email"
                     required
                   />
@@ -61,9 +57,15 @@ const Login = () => {
                   </div>
                 </div>
 
-                <div className="space-x-2">
-                  <input id="remember-me" type="checkbox" />
-                  <label htmlFor="remember-me">Remember me</label>
+                <div className="flex justify-between">
+                  <div className="space-x-2">
+                    <input id="remember-me" type="checkbox" />
+                    <label htmlFor="remember-me">Remember me</label>
+                  </div>
+
+                  <a className="text-blue-500 font-light cursor-pointer">
+                    Forgot password?
+                  </a>
                 </div>
 
                 <div>
@@ -73,9 +75,17 @@ const Login = () => {
                   >
                     LOG IN
                   </button>
-                  <a className="text-blue-500 font-light cursor-pointer">
-                    Forgot password?
-                  </a>
+
+                  <p>
+                    Don't have an Account?
+                    <Link
+                      to="/register"
+                      className="text-blue-500 cursor-pointer"
+                    >
+                      {" "}
+                      Sign up
+                    </Link>
+                  </p>
                 </div>
               </div>
             </form>
