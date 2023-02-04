1e1972a57544d504b3935f4ca4cea65b6f9e04b0
khalteck <khalidoyeneye@gmail.com>
2023-02-05 00:30:42 +0100

    delete doc left

--- src/admin/Account.jsx
+++ src/admin/Account.jsx
@@ -6,10 +6,10 @@ const Account = () => {
     <div className="w-full">
       <Sidebar />
       <div
-        className={`w-full md:w-[80%] float-right bg-white pt-[80px] md:pt-[50px] px-0 md:px-12 transition-all duration-500`}
+        className={`w-full md:w-[80%] float-right bg-sky-50 pt-[80px] md:pt-[60px] px-0 md:px-12 transition-all duration-500 text-slate-700`}
       >
-        <div className="w-full min-h-screen mb-16 px-3 font-bold text-[3rem]">
-          The admin account page
+        <div className="w-full min-h-screen mb-16 px-3 font-bold text-[1.75rem]">
+          Admin Account
         </div>
       </div>
       <ScrollToTop />
