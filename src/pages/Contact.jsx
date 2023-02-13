c7d084cfafd64f28a5e7a7285219bd8bc61d1ae3
olamiidee <osibeluwoolamide@gmail.com>
2023-02-13 19:49:24 +0100

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import ContactUs from "../components/ContactUs";
+import ContactUsPage from "../components/ContactUsPage";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ScrollToTop from "../ScrollToTop";
@@ -7,9 +7,9 @@ const Contact = () => {
   return (
     <>
       <Header />
-      <section className="w-full min-h-screen overlay bg-[#020d18]/80">
-        <div className="px-[2%] sm:px-[6%] flex justify-center items-center pt-[100px]">
-          <ContactUs />
+      <section className="w-full h-full overlay bg-[#020d18]/90">
+        <div className="flex justify-center items-center pt-[100px] px-[4%]">
+          <ContactUsPage />
         </div>
       </section>
       <Footer />
